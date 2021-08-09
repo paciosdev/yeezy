11597e1177fce11fa33cb3f1f3a6877da2f0887a
Francesco Paciello <kekkopacio@gmail.com>
2021-08-09 16:02:52 +0200

    Initial Commit

---
+++ aa/aaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aaApp.swift
+//  aa
+//
+//  Created by Kekko Paciello on 09/08/21.
+//
+
+import SwiftUI
+
+@main
+struct aaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
